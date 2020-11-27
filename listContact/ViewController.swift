c9fd4e39713033bb5d7d330a50e50af2f440a72e
sealzhk <sealzhk@users.noreply.github.com>
2020-11-27 21:26:16 +0600

    Initial Commit

---
+++ listContact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  listContact
+//
+//  Created by Alua Zhakieva on 11/27/20.
+//  Copyright © 2020 Alua Zhakieva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
